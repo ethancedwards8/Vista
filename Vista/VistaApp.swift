8bc20b13f8c2481039fe09e6dbd73a81eb818842
Ethan Edwards <ethan@ethancedwards.com>
2021-12-12 14:52:35 -0500

    init commit.

---
+++ Vista/VistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VistaApp.swift
+//  Vista
+//
+//  Created by Ethan Carter Edwards on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct VistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
